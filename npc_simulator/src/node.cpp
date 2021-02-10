b4e7525eb568b54822341ec10bf35b97f8ec68d0
Kosuke Murakami <kosuke.murakami@tier4.jp>
2021-02-10 14:14:07 +0900

    tmp rename hpp -> h
    Signed-off-by: Kosuke Murakami <kosuke.murakami@tier4.jp>

--- npc_simulator/src/node.cpp
+++ npc_simulator/src/node.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "npc_simulator/node.hpp"
+#include "npc_simulator/node.h"
 
 #include <algorithm>
 #include <limits>
